bc3543be533d3ea12fad227d54f918ce2947ec3d
jbphet <john.blanco@colorado.edu>
2018-09-10 16:56:14 -0600

    created common checkbox for projector mode, integrated into SOM, SOMB, and AI, see https://github.com/phetsims/joist/issues/342

--- js/states-of-matter-basics-main.js
+++ js/states-of-matter-basics-main.js
@@ -12,16 +12,12 @@ define( function( require ) {
   // modules
   var GlobalOptionsNode = require( 'STATES_OF_MATTER/common/view/GlobalOptionsNode' );
   var PhaseChangesScreen = require( 'STATES_OF_MATTER/phase-changes/PhaseChangesScreen' );
-  var Property = require( 'AXON/Property' );
   var Sim = require( 'JOIST/Sim' );
   var SimLauncher = require( 'JOIST/SimLauncher' );
   var StatesScreen = require( 'STATES_OF_MATTER/states/StatesScreen' );
 
   // strings
   var statesOfMatterBasicsTitleString = require( 'string!STATES_OF_MATTER_BASICS/states-of-matter-basics.title' );
-
-  // property that controls projector mode, initial value can be set via a query parameter
-  var projectorModeProperty = new Property( phet.chipper.queryParameters.colorProfile === 'projector' );
 
   var simOptions = {
     credits: {
@@ -31,7 +27,7 @@ define( function( require ) {
       qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Ethan Johnson, Oliver Orejola, Benjamin Roberts, Bryan Yoelin',
       thanks: 'Thanks to Actual Concepts for working with the PhET development team to convert this simulation to HTML5.'
     },
-    optionsNode: new GlobalOptionsNode( projectorModeProperty )
+    optionsNode: new GlobalOptionsNode()
   };
 
   SimLauncher.launch( function() {
