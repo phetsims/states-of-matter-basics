9fbdd697ad19e2646245698ecb6c30cb91babbbf
samreid <reids@colorado.edu>
2020-02-27 11:46:16 -0700

    ES6 module migration, see https://github.com/phetsims/chipper/issues/875

--- js/states-of-matter-basics-main.js
+++ js/states-of-matter-basics-main.js
@@ -6,42 +6,38 @@
  * @author Chris Malley (PixelZoom, Inc.)
  * @author Sam Reid (PhET Interactive Simulations)
  */
-define( require => {
-  'use strict';
 
-  // modules
-  const GlobalOptionsNode = require( 'STATES_OF_MATTER/common/view/GlobalOptionsNode' );
-  const PhaseChangesScreen = require( 'STATES_OF_MATTER/phase-changes/PhaseChangesScreen' );
-  const Sim = require( 'JOIST/Sim' );
-  const SimLauncher = require( 'JOIST/SimLauncher' );
-  const StatesScreen = require( 'STATES_OF_MATTER/states/StatesScreen' );
-  const Tandem = require( 'TANDEM/Tandem' );
+import Sim from '../../joist/js/Sim.js';
+import SimLauncher from '../../joist/js/SimLauncher.js';
+import GlobalOptionsNode from '../../states-of-matter/js/common/view/GlobalOptionsNode.js';
+import PhaseChangesScreen from '../../states-of-matter/js/phase-changes/PhaseChangesScreen.js';
+import StatesScreen from '../../states-of-matter/js/states/StatesScreen.js';
+import Tandem from '../../tandem/js/Tandem.js';
+import statesOfMatterBasicsStrings from './states-of-matter-basics-strings.js';
 
-  // strings
-  const statesOfMatterBasicsTitleString = require( 'string!STATES_OF_MATTER_BASICS/states-of-matter-basics.title' );
+const statesOfMatterBasicsTitleString = statesOfMatterBasicsStrings[ 'states-of-matter-basics' ].title;
 
-  // Eagerly create GlobalOptionsNode so it works smoothly with PhET-iO
-  const globalOptionsNode = new GlobalOptionsNode( Tandem.ROOT.createTandem( 'global' ).createTandem( 'view' ).createTandem( 'globalOptionsNode' ) );
+// Eagerly create GlobalOptionsNode so it works smoothly with PhET-iO
+const globalOptionsNode = new GlobalOptionsNode( Tandem.ROOT.createTandem( 'global' ).createTandem( 'view' ).createTandem( 'globalOptionsNode' ) );
 
-  const simOptions = {
-    credits: {
-      leadDesign: 'Paul Beale, Yuen-ying Carpenter, Sarah McKagan, Emily B. Moore, Noah Podolefsky,<br>Amy Rouinfar',
-      softwareDevelopment: 'John Blanco, Aaron Davis, Aadish Gupta',
-      team: 'Wendy Adams, Jack Barbera, Amy Hanson, Kelly Lancaster, Ariel Paul, Kathy Perkins,<br>Carl Wieman',
-      qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Ethan Johnson, Liam Mulhall,<br>' +
-                        'Oliver Orejola, Laura Rea, Benjamin Roberts, Jacob Romero, Kathryn Woessner, Bryan Yoelin',
-      thanks: 'Thanks to Actual Concepts for working with the PhET development team to convert this simulation to HTML5.'
-    },
+const simOptions = {
+  credits: {
+    leadDesign: 'Paul Beale, Yuen-ying Carpenter, Sarah McKagan, Emily B. Moore, Noah Podolefsky,<br>Amy Rouinfar',
+    softwareDevelopment: 'John Blanco, Aaron Davis, Aadish Gupta',
+    team: 'Wendy Adams, Jack Barbera, Amy Hanson, Kelly Lancaster, Ariel Paul, Kathy Perkins,<br>Carl Wieman',
+    qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Ethan Johnson, Liam Mulhall,<br>' +
+                      'Oliver Orejola, Laura Rea, Benjamin Roberts, Jacob Romero, Kathryn Woessner, Bryan Yoelin',
+    thanks: 'Thanks to Actual Concepts for working with the PhET development team to convert this simulation to HTML5.'
+  },
 
-    // create content for the Options dialog
-    createOptionsDialogContent: () => globalOptionsNode
-  };
+  // create content for the Options dialog
+  createOptionsDialogContent: () => globalOptionsNode
+};
 
-  SimLauncher.launch( function() {
-    const sim = new Sim( statesOfMatterBasicsTitleString, [
-      new StatesScreen( Tandem.ROOT.createTandem( 'statesScreen' ) ),
-      new PhaseChangesScreen( false, Tandem.ROOT.createTandem( 'phaseChangesScreen' ) )
-    ], simOptions );
-    sim.start();
-  } );
+SimLauncher.launch( function() {
+  const sim = new Sim( statesOfMatterBasicsTitleString, [
+    new StatesScreen( Tandem.ROOT.createTandem( 'statesScreen' ) ),
+    new PhaseChangesScreen( false, Tandem.ROOT.createTandem( 'phaseChangesScreen' ) )
+  ], simOptions );
+  sim.start();
 } );
---
+++ js/states-of-matter-basics-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import statesOfMatterBasics from './statesOfMatterBasics.js';
+
+const statesOfMatterBasicsStrings = getStringModule( 'STATES_OF_MATTER_BASICS' );
+
+statesOfMatterBasics.register( 'statesOfMatterBasicsStrings', statesOfMatterBasicsStrings );
+
+export default statesOfMatterBasicsStrings;
