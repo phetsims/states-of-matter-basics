52877834e72344be08b90d25bef504712fbc6d95
zepumph <michael.kauzmann@colorado.edu>
2020-03-16 14:41:43 -0800

    remove ResetAllButtonIO.js, https://github.com/phetsims/scenery-phet/issues/572

--- js/phet-io/states-of-matter-basics-phet-io-elements-baseline.js
+++ js/phet-io/states-of-matter-basics-phet-io-elements-baseline.js
@@ -5368,7 +5368,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "ResetAllButtonIO"
+      "phetioTypeName": "NodeIO"
     },
     "statesOfMatterBasics.phaseChangesScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
@@ -7890,7 +7890,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "ResetAllButtonIO"
+      "phetioTypeName": "NodeIO"
     },
     "statesOfMatterBasics.statesScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
