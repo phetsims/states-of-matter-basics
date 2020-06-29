e0c7696439a8da2ba59024c71705221902eb7019
samreid <reids@colorado.edu>
2020-06-29 15:09:11 -0600

    Move phetioFeatured from overrides to main code, see https://github.com/phetsims/states-of-matter/issues/303

--- js/phet-io/states-of-matter-basics-overrides.js
+++ js/phet-io/states-of-matter-basics-overrides.js
@@ -28,9 +28,6 @@ window.phet.preloads.phetio.phetioElementsOverrides =
   "statesOfMatterBasics.phaseChangesScreen.model.targetNumberOfMoleculesProperty": {
     "phetioFeatured": true
   },
-  "statesOfMatterBasics.phaseChangesScreen.model.temperatureInKelvinProperty": {
-    "phetioFeatured": true
-  },
   "statesOfMatterBasics.phaseChangesScreen.view.heaterCoolerNode.visibleProperty": {
     "phetioFeatured": true
   },
@@ -107,9 +104,6 @@ window.phet.preloads.phetio.phetioElementsOverrides =
     "phetioFeatured": true
   },
   "statesOfMatterBasics.statesScreen.model.targetNumberOfMoleculesProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.model.temperatureInKelvinProperty": {
     "phetioFeatured": true
   },
   "statesOfMatterBasics.statesScreen.view.heaterCoolerNode.visibleProperty": {
