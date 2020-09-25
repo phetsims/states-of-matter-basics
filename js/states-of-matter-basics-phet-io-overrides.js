48b1b238f837a5df4f6252eea88db6504d7a57d3
samreid <reids@colorado.edu>
2020-09-25 16:15:19 -0600

    Reformat overrides, see https://github.com/phetsims/studio/issues/196

--- js/states-of-matter-basics-phet-io-overrides.js
+++ js/states-of-matter-basics-phet-io-overrides.js
@@ -1,163 +1,163 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
-{
-   "statesOfMatterBasics.phaseChangesScreen.model.containerHeightProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.heatingCoolingAmountProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.isExplodedProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.isPlayingProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.phaseDiagramExpandedProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.pressureProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.substanceProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.targetContainerHeightProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.targetNumberOfMoleculesProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.model.temperatureInKelvinProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.heaterCoolerNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.adjustableAttractionText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.argonText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.neonText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.oxygenText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.waterText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.title.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.compositeThermometerNode.temperatureUnitsProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.compositeThermometerNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.lidNode.handleNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.lidNode.pickableProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.lidNode.pointingHandNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.pressureGaugeNode.gaugeNode.labelNode.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.pressureGaugeNode.gaugeNode.labelNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.pressureGaugeNode.readoutNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.pressureGaugeNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.phaseDiagramContainer.phaseDiagramAccordionBox.visibleProperty": {
-    "phetioFeatured": false
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.phaseDiagramContainer.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.pumpNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.returnLidButton.enabledProperty": {
-    "phetioFeatured": false
-  },
-  "statesOfMatterBasics.phaseChangesScreen.view.returnLidButton.visibleProperty": {
-    "phetioFeatured": false
-  },
-  "statesOfMatterBasics.statesScreen.model.heatingCoolingAmountProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.model.isExplodedProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.model.isPlayingProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.model.pressureProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.model.substanceProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.model.targetNumberOfMoleculesProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.model.temperatureInKelvinProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.heaterCoolerNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.radioButtonGroup.argonText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.radioButtonGroup.neonText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.radioButtonGroup.oxygenText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.radioButtonGroup.waterText.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.title.textProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.particleContainerNode.compositeThermometerNode.temperatureUnitsProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.particleContainerNode.compositeThermometerNode.visibleProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.gasSelectedProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.liquidSelectedProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.solidSelectedProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.stateProperty": {
-    "phetioFeatured": true
-  },
-  "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.visibleProperty": {
-    "phetioFeatured": true
-  }
-};
+  {
+    "statesOfMatterBasics.phaseChangesScreen.model.containerHeightProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.heatingCoolingAmountProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.isExplodedProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.isPlayingProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.phaseDiagramExpandedProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.pressureProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.substanceProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.targetContainerHeightProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.targetNumberOfMoleculesProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.model.temperatureInKelvinProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.heaterCoolerNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.adjustableAttractionText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.argonText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.neonText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.oxygenText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.radioButtonGroup.waterText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.title.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.moleculesControlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.compositeThermometerNode.temperatureUnitsProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.compositeThermometerNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.lidNode.handleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.lidNode.pickableProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.lidNode.pointingHandNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.pressureGaugeNode.gaugeNode.labelNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.pressureGaugeNode.gaugeNode.labelNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.pressureGaugeNode.readoutNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.particleContainerNode.pressureGaugeNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.phaseDiagramContainer.phaseDiagramAccordionBox.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.phaseDiagramContainer.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.pumpNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.returnLidButton.enabledProperty": {
+      "phetioFeatured": false
+    },
+    "statesOfMatterBasics.phaseChangesScreen.view.returnLidButton.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "statesOfMatterBasics.statesScreen.model.heatingCoolingAmountProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.model.isExplodedProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.model.isPlayingProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.model.pressureProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.model.substanceProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.model.targetNumberOfMoleculesProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.model.temperatureInKelvinProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.heaterCoolerNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.radioButtonGroup.argonText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.radioButtonGroup.neonText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.radioButtonGroup.oxygenText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.radioButtonGroup.waterText.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.title.textProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.moleculesControlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.particleContainerNode.compositeThermometerNode.temperatureUnitsProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.particleContainerNode.compositeThermometerNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.gasSelectedProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.liquidSelectedProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.solidSelectedProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.stateProperty": {
+      "phetioFeatured": true
+    },
+    "statesOfMatterBasics.statesScreen.view.solidLiquidGasPhaseControlNode.visibleProperty": {
+      "phetioFeatured": true
+    }
+  };
